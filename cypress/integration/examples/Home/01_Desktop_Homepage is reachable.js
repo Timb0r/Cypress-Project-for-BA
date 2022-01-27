f14676b531dbb55ebdea07105bc642d58d92a360
Timb0r <timberland16@web.de>
2022-01-27 23:34:14 +0100

    test on cookie save in local storage

--- cypress/integration/examples/Home/01_Desktop_Homepage is reachable.js
+++ cypress/integration/examples/Home/01_Desktop_Homepage is reachable.js
@@ -1,8 +1,7 @@
 /// <reference types="cypress" />
 describe('Home page is reachable', () => {
-  it('visits website', () => {
-
-    cy.visit('https://cy-borg.net/hortt-dev/'),
-    cy.wait(200);
+    it('visits website', () => {
+        cy.visit('https://cy-borg.net/hortt-dev/')
+        cy.wait(200)
     })
-  });
+})
--- cypress/integration/examples/Home/03_cookies control.spec.js
+++ cypress/integration/examples/Home/03_cookies control.spec.js
@@ -1,18 +1,18 @@
 /// <reference types="cypress" />
 
 context('click cookie banner, check cookies', () => {
-  beforeEach(() => {
-    Cypress.Cookies.debug(true)
+    beforeEach(() => {
+        Cypress.Cookies.debug(true)
 
-    cy.visit('https://cy-borg.net/hortt-dev/#/')
-  })
+        cy.visit('https://cy-borg.net/hortt-dev/#/')
+    })
 
-  it("should be null first time page is visited", () => {
-    cy.getLocalStorage("cookie:accepted").should("equal", null);
-  });
+    it('should be null first time page is visited', () => {
+        cy.getLocalStorage('cookie:accepted').should('equal', null)
+    })
 
-  it("should be true after clicking cookies button", () => {
-    cy.get('.cookieBanner > .btn').click();
-    cy.getLocalStorage("cookie:accepted").should("equal", "true");
-  });
+    it('should be true after clicking cookies button', () => {
+        cy.get('.cookieBanner > .btn').click()
+        cy.getLocalStorage('cookie:accepted').should('equal', 'true')
+    })
 })

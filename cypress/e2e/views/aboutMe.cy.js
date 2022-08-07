c4729ec86340b7d92fec1ac7a587df09f2433ac4
Timb0r <timberland16@web.de>
2022-08-07 20:13:26 +0200

    optimize aboutMe page

--- cypress/e2e/views/aboutMe.cy.js
+++ cypress/e2e/views/aboutMe.cy.js
@@ -1,7 +1,9 @@
+import selectors from '../constants/selectors';
 import { routes } from '../constants/url';
 
 Cypress.Commands.add('inContent', (callback) => {
     cy.get('main').within(callback);
+    // how to efficiently use selectors.contentcard and contentrow in context to check all the contents without failing?
 });
 
 describe('Check aboutme page and content', () => {
@@ -10,25 +12,27 @@ describe('Check aboutme page and content', () => {
     });
     it('check title', () => {
         cy.inContent(() => {
-            cy.get('h1')
-                .should('contain', 'Lernen Sie mich kennen')
-                .and('be.visible');
+            // should use selectors.headline to check outer element headline AND h1 but doesn´t work yet
+            cy.get(selectors.h1).should(
+                'contain',
+                'Lernen Sie mich kennen',
+                // be.visible deleted in all checks becaus checking text in source code and visibility in viewport is the same?
+            );
         });
     });
     context('check content box elements', () => {
         it('check name headline and subheadline', () => {
             cy.inContent(() => {
-                cy.get('h2')
-                    .should('contain', 'Manfred Hortt')
-                    .and('be.visible');
-                cy.contains('Ihr Berater in Köln und Umgebung').should(
-                    'be.visible',
+                cy.get(selectors.h2).should('contain', 'Manfred Hortt');
+                cy.get(selectors.subHeadline).should(
+                    'contain',
+                    'Ihr Berater in Köln und Umgebung',
                 );
             });
         });
         it('check image', () => {
             cy.inContent(() => {
-                cy.get('img.aboutMe__image')
+                cy.get('img.aboutMe__image') // no data component
                     .should('be.visible')
                     .should('have.attr', 'src', 'img/aboutMe.jpg')
                     .should('have.attr', 'alt', 'hortt_aboutMe');
@@ -36,30 +40,10 @@ describe('Check aboutme page and content', () => {
         });
         it('check "Eckdaten" section', () => {
             cy.inContent(() => {
-                // iteriert lösen und datacomponents nutzen
                 cy.contains('Ein paar Eckdaten zu mir:').should('be.visible');
-                cy.get('ul.aboutMe__basicInfoList');
-                cy.contains('li', 'Verheiratet, Vater von zwei Kindern').should(
-                    'be.visible',
-                );
-                cy.contains('li', 'Ausbildung zum Bankkaufmann').should(
-                    'be.visible',
-                );
-                cy.contains('li', 'Studium zum Bankfachwirt').should(
-                    'be.visible',
-                );
-                cy.contains(
-                    'li',
-                    'Fachberater für Baufinanzierungen und komplexe Finanzierungen als Prokurist in der Commerzbank (1984- 2012)',
-                ).should('be.visible');
-                cy.contains(
-                    'li',
-                    'Senior-Spezialberater Baufinanzierung im Privat- und Geschäftsbankenbereich sowie im Kooperationsgeschäft der Deutschen Bank (2012-2019)',
-                ).should('be.visible');
-                cy.contains(
-                    'li',
-                    'Seit 2020 selbständiger Baufinanzierungsberater',
-                ).should('be.visible');
+                cy.get('ul.aboutMe__basicInfoList')
+                    .children()
+                    .should('have.length', 6);
             });
         });
     });

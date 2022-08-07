9c82f0018ef6ce91a64d1a574716adf00e8c1281
Timb0r <timberland16@web.de>
2022-08-07 16:05:59 +0200

    fix favicon and metatitle test

--- cypress/e2e/components/Favicon_and_metatitle.cy.js
+++ cypress/e2e/components/Favicon_and_metatitle.cy.js
@@ -9,12 +9,18 @@ describe('Check favicon and website title in browser tab', () => {
     beforeEach(() => {
         cy.visit(routes.home);
     });
-    //Check browsertab for correct favicon and title
     it('check favicon', () => {
         cy.inHead(() => {
-            cy.contains('head')
-                .should('have.attr', 'href', 'favicon.ico')
-                .and('be.visible');
+            const link = cy.get('link').first();
+            link.should('have.attr', 'href', 'favicon.ico');
+        });
+    });
+    it('check metatitle', () => {
+        cy.inHead(() => {
+            cy.get('title').should(
+                'contain',
+                'Manfred Hortt - Bankfachwirt & Baufinanzierung',
+            );
         });
     });
 });
